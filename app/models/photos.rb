8ae21ebfb7deb43db9230c40ebaa9200cef305a6
BWheatie <BWheatie@gmail.com>
2016-12-03 21:33:31 -0600

    Attachment validation

---
+++ app/models/photos.rb
@@ -0,0 +1,5 @@
+class Photo < ActiveRecord::Base
+    has_attached_file :image
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
+end
